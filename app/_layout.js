89c1edbc05a7a996011da1f7676accac383cec47
Mariah Shannen Sumaria <sumariamariahshannen@email.com>
2025-12-02 22:22:33 +0800

    toast delay i change into 10000 (10sec)

--- app/_layout.js
+++ app/_layout.js
@@ -17,7 +17,7 @@ export default function RootLayout() {
       <Stack screenOptions={{ headerShown: false }} />
 
       {/* Global Toast for notifications */}
-      <Toast position="top" topOffset={60} visibilityTime={3000} />
+      <Toast position="top" topOffset={60} visibilityTime={10000} />
     </>
   );
 }
